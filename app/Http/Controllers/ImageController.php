f435d526471caebf64747a6b153e90c27d918040
CarlosDaniel <105173667+Carlos-Daniel-Dev@users.noreply.github.com>
2024-02-10 10:36:12 -0300

    ImageController

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ImageController extends Controller
+{
+
+    public function upload()
+    {
+        
+    }
+
+    public function store(Request $request)
+    {
+        
+    }
+
+
+    public function show(string $id)
+    {
+        
+    }
+}
