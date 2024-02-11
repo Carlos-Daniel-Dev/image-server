35f7bd3a4eb5050ef7eb9a5e49153f5ec7814c01
CarlosDaniel <105173667+Carlos-Daniel-Dev@users.noreply.github.com>
2024-02-10 21:26:22 -0300

    show now is a view and not just a return image method

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,16 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-use App\Http\Models\Image;
-
 use Illuminate\Support\Facades\Storage;
-
-
+use Illuminate\Support\Facades\URL;
 
 class ImageController extends Controller
 {
-
     public function upload()
     {
         return view('upload');
@@ -20,7 +15,9 @@ class ImageController extends Controller
 
     public function store(Request $request)
     {
-        if (! $request->hasFile('image')) return redirect()->route('images.upload')->with('error', 'image uploaded with error');
+        if (!$request->hasFile('image')) {
+            return redirect()->route('images.upload')->with('error', 'image uploaded with error');
+        }
 
         $image = $request->file('image');
 
@@ -31,16 +28,15 @@ class ImageController extends Controller
         return redirect()->route('images.show', ['id' => $imageName]);
     }
 
-
     public function show(Request $request, string $id)
     {
         if (Storage::exists('public/images/' . $id)) {
-            // Retorna a imagem como resposta HTTP
-            return response()->file(Storage::path('public/images/' . $id));
+            $filePath = 'public/images/' . $id;
+            $url = URL::to(Storage::url($filePath));
+            
+            return view('show', ['url' => $url]);
         } else {
-            // Retorna uma resposta de erro caso a imagem não exista
             abort(404);
         }
     }
-
 }
