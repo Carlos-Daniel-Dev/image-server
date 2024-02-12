831b92247b054fa62cc7883bbadb29e8257d4e53
CarlosDaniel <105173667+Carlos-Daniel-Dev@users.noreply.github.com>
2024-02-12 17:16:07 -0300

    creating and applying templates

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,24 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Image</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <style>
-        img {
-            max-width: 100%;
-            max-height: 100%;
-            display: block;
-            margin: auto;
-        }
-    </style>
-</head>
-<body class="bg-dark">
-    <div class="container-xl d-flex justify-content-center align-items-center vh-100">
-        <div class="col-6 text-center">
-            <img src="{{ $url }}">
-        </div>
+@extends('templates.main')
+
+
+section('content')
+<div class="container-xl d-flex justify-content-center align-items-center vh-100">
+    <div class="col-6 text-center">
+        <img src="{{ $url }}">
     </div>
-</body>
-</html>
+</div>
+@endsection
--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Images Upload</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-</head>
-<body class="bg-dark">
+@extends('templates.main')
 
+@section('content')
 <div class="container-xl d-flex justify-content-center align-items-center vh-100">
     <div class="row">
         <div class="col-md-12 mx-auto">
@@ -56,6 +49,4 @@
         reader.readAsDataURL(input.files[0]);
     }
 </script>
-
-</body>
-</html>
+@endsection
