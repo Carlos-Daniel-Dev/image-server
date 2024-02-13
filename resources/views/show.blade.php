5960e8b5b65ad76cf7b41ac07b8a864c2f95e333
CarlosDaniel <105173667+Carlos-Daniel-Dev@users.noreply.github.com>
2024-02-13 12:50:53 -0300

    fixing title h1 and image size in show page

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,10 +3,11 @@
 @section('title') {{ $title }} @endsection
 
 @section('content')
+<header class="mt-4">
+    <h1 class="text-center">{{ $title }}</h1>
+</header>
 
-<div class="container-xl d-flex justify-content-center align-items-center vh-100">
-    <div class="col-6 text-center">
-        <img src="{{ $url }}">
-    </div>
+<div class="container-xl d-flex justify-content-center align-items-center" style="min-height: 70vh;">
+    <img src="{{ $url }}" class="img-fluid imagem">
 </div>
 @endsection
--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -6,12 +6,16 @@
     <title>@yield('title')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
-        img {
-            max-width: 100%;
-            max-height: 100%;
-            display: block;
-            margin: auto;
+        .imagem {
+          max-width: 100%; /* Garante que a imagem não exceda o tamanho do contêiner */
+          max-height: 80vh; /* Define a altura máxima como 80% da altura da janela de visualização */
+          margin: auto; /* Centraliza a imagem horizontalmente */
+          display: block; /* Garante que a margem automática funcione */
         }
+
+        body {
+            font-family: 'Roboto', sans-serif; /* Altera a fonte padrão para 'Roboto' */
+            color: white;        }
     </style>
 </head>
 <body class="bg-dark">
