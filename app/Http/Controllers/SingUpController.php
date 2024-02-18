88b81d29d4e51b42ef83dcf20c68aea11c31e712
CarlosDaniel <105173667+Carlos-Daniel-Dev@users.noreply.github.com>
2024-02-18 11:35:24 -0300

    wrong path to UserModel fixed

--- app/Http/Controllers/SingUpController.php
+++ app/Http/Controllers/SingUpController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\User;
+
 class SingUpController extends Controller
 {
     public function index ()
@@ -11,12 +13,23 @@ class SingUpController extends Controller
         return view('singup');
     }
 
-    public function store ()
+    public function store (Request $request)
     {
         $validated = $request->validate([
             'username' => 'required|min:5|max:24',
             'email' => 'required|email',
             'password' => 'required|min:5|max:24'
         ]);
+
+        User::create([
+
+            'username' => $validated->input('username'),
+            'email' => $validated->input('email'),
+            'password' => $validated->input('password'),
+
+        ]);
+
+        return 'opa';
+
     }
 }
