8ee70b272c14c6415270e51773ca93d021557d3c
CarlosDaniel <105173667+Carlos-Daniel-Dev@users.noreply.github.com>
2024-02-19 11:43:03 -0300

    login route done

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,11 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\User;
 
 class LoginController extends Controller
 {
     public function index ()
     {
         return view('login');
+    }
+
+    public function login (Request $request)
+    {
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:5|max:24'
+        ]);
+
+        $email = $validated['email'];
+        $password = $validated['password'];
+
+        $user = User::where('email', $email)->first();
+
+        if ($user && Hash::check($password, $user->password)) {
+            return redirect()->route('home')->withCookie(cookie('user_token', $token, 60*24*365));
+        } else {
+            return redirect()->route('login');
+        }
+
     }
 }
