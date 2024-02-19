2a5f2b7bfe3df1facc232090b5c7e4ba3052fcc4
CarlosDaniel <105173667+Carlos-Daniel-Dev@users.noreply.github.com>
2024-02-19 17:06:05 -0300

    now have a confirm password field in singup

--- resources/views/singup.blade.php
+++ resources/views/singup.blade.php
@@ -40,6 +40,10 @@
                             <label for="exampleInputPassword1">Password</label>
                             <input type="password" class="form-control" name="password" placeholder="Password">
                         </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Confirm Password</label>
+                            <input type="password" class="form-control" name="confirm_password" placeholder="Confirm Password">
+                        </div>
                         <button type="submit" class="btn btn-primary">SingUp</button>
                     </form>
                 </div>
